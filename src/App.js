846d91c6ccfe4bbe197715b161018feb6dcefb37
VedShirgaonkar <vedshirgaonkar@gmail.com>
2021-09-23 16:38:49 +0530

    r

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
+import React  from 'react'
+import { useState,useEffect } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const getLocalItems = () => {
+  let list = localStorage.getItem('lists');
+  console.log(list);
+  if (list) {
+      return JSON.parse(localStorage.getItem('lists'));
+  } else {
+      return [];
+  }
 }
+
+
+function App() {
+ 
+  const [todo, setTodo] = useState(getLocalItems());
+  const [input,setInput]=useState('');
+  
+  const AddTodo = (event)=>{
+   // alert('I m working');
+   event.preventDefault();
+
+    
+    //let cl = document.getElementById('myInp').value;
+    if(input === ''){
+      alert("enter data");
+    }
+    else{
+      setTodo([...todo,input]);
+    }
+    
+  }
+  
+  useEffect(() => {
+    // storing input name
+    localStorage.setItem("lists",JSON.stringify(todo));
+  }, [todo]);
+
+  function ClrTodo(){
+    setTodo([]);
+  }
+    return (
+
+       <div className = "App">
+        
+         
+         <div className="myData">
+         <h1>Todo App</h1>
+         <input id="myInp" value={input} onChange={event=>setInput(event.target.value)}/> 
+          <div className="FORM">
+         <button type="submit" onClick={AddTodo}>AddTodo</button>
+         <button type="submit" onClick={ClrTodo}>ClearTodo</button>
+
+         </div>
+         <div className = "MyApp">
+         <ul>
+          {todo.map(i=>(
+            <li>{i}</li>
+          ))}
+         </ul>
+         </div>
+         </div>
+         
+
+        </div>
+    );
+}
+/*
+function App() {
+ 
+  const [todo, setTodo] = useState(() => {
+    // getting stored value
+    const saved = localStorage.getItem("todo");
+    const initialValue = JSON.parse(saved);
+    return initialValue || "";
+  });
+  const [input,setInput]=useState('');
+  
+  const AddTodo = (event)=>{
+   // alert('I m working');
+   event.preventDefault();
+    setTodo([...todo,input]);
+    
+
+    
+  }
+  
+  useEffect(() => {
+    // storing input name
+    localStorage.setItem("todo",JSON.stringify(todo));
+  }, [todo]);
+    return (
+
+       <div className = "App">
+        
+         <h1>Todo App</h1>
+         <input id="myInp" value={input} onChange={event=>setInput(event.target.value)}/> 
+         <form>
+         <button type="submit" onClick={AddTodo}>AddTodo</button>
+         </form>
+         
+         <ul>
+          {todo.map(i=>(
+            <li>{i}</li>
+          ))}
+          
+
+         </ul>
+
+        </div>
+    );
+}*/
 
 export default App;
